fca6e585402dcce16f03d08689e61f405f4b6985
fruttz <dito.arjwasa@gmail.com>
2023-02-22 17:44:06 +0700

    added fish data

---
+++ Amis Village Game Design/src/Scenes/Fish/FishData.gd
@@ -0,0 +1,54 @@
+extends Node
+
+var fish_data = [
+	{
+		"id": 1,
+		"name": "Tuna",
+		"rarity": 1,
+		"difficulty": 1,
+		"affection": 3
+	},
+	{
+		"id": 2,
+		"name": "Gurita",
+		"rarity": 4,
+		"difficulty": 5,
+		"affection": 5
+	},
+	{
+		"id": 3,
+		"name": "Letter Enam",
+		"rarity": 5,
+		"difficulty": 5,
+		"affection": 6
+	},
+	{
+		"id": 4,
+		"name": "Kepiting",
+		"rarity": 3,
+		"difficulty": 4,
+		"affection": 5
+	},
+	{
+		"id": 5,
+		"name": "Kecebong",
+		"rarity": 1,
+		"difficulty": 3,
+		"affection": 1
+	},
+	{
+		"id": 6,
+		"name": "Arwana",
+		"rarity": 2,
+		"difficulty": 2,
+		"affection": 4
+	},
+	{
+		"id": 7,
+		"name": "Kura-Kura",
+		"rarity": 3,
+		"difficulty": 2,
+		"affection": 5
+	}
+]
+
