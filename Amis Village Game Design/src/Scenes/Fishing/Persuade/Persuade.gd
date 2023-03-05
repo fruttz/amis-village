eb03e342b9780aa54317e14aa203be22418e8ca4
solace6282 <gkataro4@gmail.com>
2023-03-05 15:48:31 +0700

    stuff

--- Amis Village Game Design/src/Scenes/Fishing/Persuade/Persuade.gd
+++ Amis Village Game Design/src/Scenes/Fishing/Persuade/Persuade.gd
@@ -12,25 +12,24 @@ func _ready():
 	persuade()
 	pass # Replace with function body.
 
+#func _process(delta):
+#	$Control/ProgressBar.value += 1
+#	pass
+
 func persuade():
-#	print(PersuasionFish.curr_fish+"_persuade")
 	var dialog = Dialogic.start(get_parent().fish["fish_name"]+"_persuade")
 #	var dialog = Dialogic.start("Kepiting_persuade")
 	dialog.pause_mode = PAUSE_MODE_PROCESS
-#	var dialog = Dialogic.start("fish_01_persuade")
-#	dialog.connect("timeline_end",self,"end_dialog")
 	dialog.connect("dialogic_signal",self,"result_fish")
 	add_child(dialog)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
 
 func result_fish(param):
 	if param == "success":
-		# increase fish function
 		Inventory.updateAffection(get_parent().fish, 1)
-#		print("Got "+get_parent().fish["fish_name"])
 	else:
 		print("You failed to catch "+get_parent().fish["fish_name"])
 	get_tree().change_scene("res://src/Scenes/Environment/Waters Test.tscn")
 	pass
+	
