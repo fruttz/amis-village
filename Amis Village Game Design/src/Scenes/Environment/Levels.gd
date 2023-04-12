fabc04a3e020f641ebeda527f87eb93af6e69a51
fruttz <dito.arjwasa@gmail.com>
2023-04-12 21:13:53 +0700

    added background to beachscene

--- Amis Village Game Design/src/Scenes/Environment/Levels.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-func _ready():
-	# Set initial player position of the player same as the Position2D node
-	if GlobalPosition.previous_level != null:
-		$Player.set_position(get_node(GlobalPosition.previous_level + "Pos").position)
