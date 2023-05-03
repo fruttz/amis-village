109765b3f0dde458babe579af587ade2bb0638ed
solace6282 <gkataro4@gmail.com>
2023-05-03 13:51:20 +0700

    extra npc for checking multiplication

--- Amis Village Game Design/src/Scenes/NPC/NPC.gd
+++ Amis Village Game Design/src/Scenes/NPC/NPC.gd
@@ -9,13 +9,14 @@ export var npc_name = "pakle"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	initNPC()
+
+func initNPC():
 	var sprite_name = "res://src/Assets/NPC/"+ npc_name +".png"
 	$Sprite.texture = load(sprite_name)
 	$Sprite.scale.x = 0.06
 	$Sprite.scale.y = 0.06
 	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
