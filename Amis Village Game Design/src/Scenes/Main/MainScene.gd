26bd4da2b91307b8027d1ae60d5469912b78f1ef
fruttz <dito.arjwasa@gmail.com>
2023-05-16 00:51:20 +0700

    added bgm and some minor tweaks

--- Amis Village Game Design/src/Scenes/Main/MainScene.gd
+++ Amis Village Game Design/src/Scenes/Main/MainScene.gd
@@ -8,26 +8,4 @@ signal open_inv
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-#	if Input.is_action_just_released("ui_cancel"):
-#		if $Pause.is_visible():
-##			get_tree().paused = false
-#			$Pause.hide()
-#		else:
-##			get_tree().paused = true
-#			$Pause.show()
-#
-	if Input.is_action_just_released("inventory"):
-		print("xdd")
-		if $Node.is_visible():
-#			get_tree().paused = false
-			$Node.hide()
-		else:
-			emit_signal("open_inv")
-#			get_tree().paused = true
-			$Node.show()
-	pass
+	get_tree().paused = true
