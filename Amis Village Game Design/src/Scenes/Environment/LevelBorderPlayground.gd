ffdd6e192c1042e7599d461ff1d67f083c649f23
solace6282 <gkataro4@gmail.com>
2023-05-15 22:55:19 +0700

    fix inventory issue

--- Amis Village Game Design/src/Scenes/Environment/LevelBorderPlayground.gd
+++ Amis Village Game Design/src/Scenes/Environment/LevelBorderPlayground.gd
@@ -4,5 +4,4 @@ func _on_LevelBorderPlayground_body_entered(_body):
 # warning-ignore:return_value_discarded
 	Globals.previous_level = get_parent().name
 	get_tree().change_scene("res://src/Scenes/Environment/PlaygroundScene.tscn")
-
-
+	
